46379122b8e0e29126dce3136c0acc51c8c0e91d
Melissa Gu <melissa.gu@richmond.edu>
2018-03-03 14:51:01 -0500

    fixing event

--- app/src/main/java/com/example/ryanjennings/spiderscheduler/Dashboard.java
+++ app/src/main/java/com/example/ryanjennings/spiderscheduler/Dashboard.java
@@ -7,13 +7,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.ListView;
+import android.widget.ArrayAdapter;
 
+import android.util.Log;
 
 import java.util.*;
 import java.io.*;
 
 public class Dashboard extends AppCompatActivity {
 
+    private TextView mTextMessage;
     private ListView mListView;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -46,13 +49,24 @@ public class Dashboard extends AppCompatActivity {
 
         mListView = (ListView) findViewById(R.id.event_list_view);
 // 1
-        final ArrayList<Event> eventList = Event.readCSV("recipes.json", this);
+        ArrayList<Event> eventList = new ArrayList<Event>();
+        Log.d("warning","before");
+        try {
+            eventList = Event.readCSV("events.csv", this);
+            Log.d("warning","in this block");
+        }
+        catch(Exception E) {
+            Log.d("warning","error");
+        }
 // 2
         String[] listItems = new String[eventList.size()];
+
 // 3
+        Log.d("warning",(" " + eventList.size()));
         for(int i = 0; i < eventList.size(); i++){
             Event event = eventList.get(i);
             listItems[i] = event.event_name;
+            Log.d("warning",event.event_name);
         }
 // 4
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
--- app/src/main/java/com/example/ryanjennings/spiderscheduler/Event.java
+++ app/src/main/java/com/example/ryanjennings/spiderscheduler/Event.java
@@ -8,6 +8,8 @@ import java.io.*;
 import java.text.*;
 
 import java.time.format.DateTimeFormatter;
+import android.util.Log;
+import android.content.Context;
 
 /**
  * Created by ryanjennings on 3/3/18.
@@ -37,22 +39,26 @@ public class Event {
     // Format: Type(int), organization(str), eventName(str)
     // location(str), date(Date),time_start(Time),time_end(Time)
     // @TargetApi(Build.VERSION_CODES.M)
-    public static ArrayList<Event> readCSV(String fileName) throws FileNotFoundException, ParseException{
+    public static ArrayList<Event> readCSV(String fileName, Context context) throws IOException, ParseException{
         ArrayList<Event> events = new ArrayList<Event>();
-        File file = new File(fileName);
-        Scanner sc = new Scanner(file);
-        sc.nextLine();
-        while(sc.hasNextLine()){
-            String line = sc.nextLine();
+        Log.d("warning","inside readCSV");
+        InputStream is = context.getResources().openRawResource(R.raw.events);
+        BufferedReader sc = new BufferedReader(new InputStreamReader(is));
+        String line = sc.readLine();
+        Log.e("warning","inside readCSV 2");
+        while(line != null){
             String[] tokens = line.split(",");
             int type = Integer.parseInt(tokens[0]);
-            DateFormat format = new SimpleDateFormat("MM-DD-YY", Locale.ENGLISH);
+            DateFormat format = new SimpleDateFormat("MM-dd-yy", Locale.ENGLISH);
             Date date = format.parse(tokens[4]);
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("h:m a");
             LocalTime time_start = LocalTime.parse(tokens[5], DateTimeFormatter.ofPattern("h:m a"));
             LocalTime time_end = LocalTime.parse(tokens[6], DateTimeFormatter.ofPattern("h:m a"));
             Event event = new Event(type, tokens[1], tokens[2], tokens[3], date, time_start, time_end);
             events.add(event);
+            Log.e("warning",line);
+            Log.e("warning",event.event_name);
+            line = sc.readLine();
         }
         sc.close();
         return events;
